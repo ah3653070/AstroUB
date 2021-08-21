5469612b2793d6b1cf2e0e364caf404161e5ad57
ʟ๏ᴠᴇʀ ◠‿◕ <79841865+loverboyXD@users.noreply.github.com>
2021-08-21 13:01:52 +0530

    «Astro-plugins»

---
+++ astro/plugins/__init__.py
@@ -0,0 +1,33 @@
+from astro.config import Config 
+from astro import bot 
+from astro import vision 
+
+BOT_USERNAME = Config.BOT_USERNAME
+OWNER_USERNAME = Config.OWNER_USERNAME
+ASTRO = "@Astro_UserBot"
+# Oh Yes...
+if Config.SUDO_USERS:
+    sudo = "YES Have Sudo"
+else:
+    sudo = "Nope No Sudo"
+if Config.PMSECURITY.lower() == "off":
+    pm = "DE-ACTIVE"
+else:
+    pm = "ACTIVE"
+
+astro = f"**ƛsτʀ๏ Vision:** {vision}\n"
+astro += f"**SUDO USERS:** {sudo}\n"
+astro += f"**PM SECURITY:** {pm}\n"
+astro += f"**Assistant:** {BOT_USERNAME}\n"
+astro += f"__My Master__: {OWNER_USERNAME}\n"
+astro += f"__Protected by__: {ASTRO}\n\n"
+
+astrostats = f"{astro}"
+
+
+# FOR PING LITTLE SETUP 
+ASTRO = bot.me.first_name
+OWNER_ID = bot.me.id
+
+# PYTHON VISION 
+PYTHON = "3.9.6"
---
+++ astro/plugins/alive.py
@@ -0,0 +1,85 @@
+import time
+from datetime import datetime
+from io import BytesIO
+import requests
+from astro.plugins import PYTHON
+from astro import bot, vision, StartTime
+from astro import CMD_HELP
+from astro.config import Config 
+
+# Configs # 
+NAME = Config.NAME
+A_PIC = Config.A_PIC if Config.A_PIC else "https://telegra.ph/file/5979e313d5d43fdf40ec7.jpg"
+A_TEXT = Config.A_TEXT if Config.A_TEXT else " This is ƛsτʀ๏\n   Ready in your protection"
+emoji = "**❅**" 
+emoji2 = "༺"
+emoji3 = "༻"
+
+# This is 4 later Purpose # 
+
+if Config.SUDO_USERS:
+    sudo = "Enabled"
+else:
+    sudo = "Disabled"
+# uptime 
+def get_readable_time(seconds: int) -> str:
+    count = 0
+    ping_time = ""
+    time_list = []
+    time_suffix_list = ["s", "m", "h", "days"]
+
+    while count < 4:
+        count += 1
+        if count < 3:
+            remainder, result = divmod(seconds, 60)
+        else:
+              remainder, result = divmod(seconds, 24)
+        if seconds == 0 and remainder == 0:
+            break
+        time_list.append(int(result))
+        seconds = int(remainder)
+
+    for x in range(len(time_list)):
+        time_list[x] = str(time_list[x]) + time_suffix_list[x]
+    if len(time_list) == 4:
+        ping_time += time_list.pop() + ", "
+      
+    time_list.reverse()
+    ping_time += ":".join(time_list)
+
+    return ping_time
+ 
+ 
+MYUSER = str(NAME) if Config.NAME else "ASTRO User✨"
+
+@astro.on(admin_cmd(pattern="alive"))
+@astro.on(sudo_cmd(pattern="alive", allow_sudo=True))
+async def amireallyalive(alive):
+    
+    start = datetime.now()
+    myid = bot.uid
+    # By @Alone_loverBoy
+    end = datetime.now()
+    (end - start).microseconds / 1000
+    uptime = get_readable_time((time.time() - StartTime))
+    astro = f"**『• Welcome To ƛsτʀ๏ •』**\n   "
+    astro += f"**{A_TEXT}**\n\n     "
+    astro += f"{emoji2}**iɲƒ๏ σƒ ƛsτʀ๏**{emoji3}\n"
+    astro += f"{emoji}** ƛsτʀ๏  Vision** ⊳≫ `{vision}`\n"
+    astro += f"{emoji}** Python Vision** ⊳≫  `{PYTHON}`\n"
+    astro += f"{emoji}** ƛsτʀ๏ uptime** ⊳≫ `{uptime}`\n"
+    astro += f"{emoji}** SUDO USER** ⊳≫ `{sudo}`\n"
+    astro += f"{emoji}** ƛsτʀ๏ Support** ⊳≫ [Astro Support](https://t.me/Astro_HelpChat)\n"
+    astro += f"{emoji}** мy мαsтєя** ⊳≫ [{MYUSER}](tg://user?id={myid})\n\n"
+    astro += f"{emoji}**✨Repository✨** ⊳≫ [GITHUB Repository✨](https://github.com/loverboyXD/AstroUB)"
+    
+    await alive.get_chat()
+    await alive.delete()
+
+    await borg.send_file(alive.chat_id, A_PIC, caption=astro, link_preview=False)
+    await alive.delete()
+
+CMD_HELP.update({"alive": "→ `.alive`\nUse - Check if your bot is working."})
+
+# ASTRO-UserBot
+# © @Alone_loverboy
